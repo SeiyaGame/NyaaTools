028716806429c1ccda49f4492d59b0a201afe30f
SeiyaGame <flavien.2001a@gmail.com>
2023-01-26 22:27:23 +0100

    Upload project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Nyaa.si Download all button
+
+> This script is largely inspired by this script that you can find [here](https://github.com/Johnsen92/nyaa_downloader)
+
+> Import the script in `Violentmonkey` to use it and have fun!
+
+The script adds a button "Download" next to the top right nav bar which on-click emulates
+a mouse click on all selected torrent links appearing in the search results on this searchpage. This enables the user to download multiple torrents at once instead of clicking on every link individually.
+
+## Use-case
+The user wants to download e.g. all episodes of a single anime. So he would refine his search
+parameters to ultimately only having all the episodes of the desired anime from one fansubber group with one resolution
+in his search results and then clicks "Download" to import all at once into his torrent client.
+
+E.g. for search parameters: Category = Anime - English-translated, Search = "Kimetsu no Yaiba horrible 1080" and then click 
+on "Download" to download all selected torrents at once.
---
+++ nyaa_download_torrents.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name        NyaaTools
+// @description Adds a "Download" button which downloads all selected torrents and more tweeks
+// @author      SeiyaGame
+// @homepage    https://github.com/SeiyaGame/NyaaTools
+// @updateURL   https://raw.githubusercontent.com/SeiyaGame/NyaaTools/main/nyaa_download_torrents.js
+// @downloadURL https://raw.githubusercontent.com/SeiyaGame/NyaaTools/main/nyaa_download_torrents.js
+// @version     1.5.0
+// @grant       none
+// @require     https://code.jquery.com/jquery-3.5.1.min.js
+// @include     *nyaa.si/*
+// @exclude     *nyaa.si/rules
+// @exclude     *nyaa.si/help
+// @exclude     *nyaa.si/upload
+// ==/UserScript==
+
+function torrentAll() {
+    const $torrentRows = $("table.torrent-list tbody tr:has(.ckbox:checked)");
+
+    $torrentRows.each(function() {
+        const $downloadLink = $("a[href*='/download']", this);
+        const $viewLink = $("a[href*='/view']", this);
+
+        if (!$downloadLink.length || !$viewLink.length) return;
+        const $link = $downloadLink.attr("href");
+        const $filename = $viewLink.attr("title") + ".torrent";
+
+        fetch($link)
+            .then(response => response.blob())
+            .then(blob => {
+                const $a = document.createElement('a');
+                $a.href = URL.createObjectURL(blob);
+                $a.download = $filename;
+                document.body.appendChild($a);
+                $a.click();
+                $a.remove();
+            });
+    });
+}
+
+// Add checkbox to table header
+$("table.torrent-list thead tr").prepend("<th class='hdr-select text-center' style='width: 10px'><input type='checkbox' id='select-all'></th>");
+$("table.torrent-list tbody tr").each(function(){
+	$(this).prepend("<td class='row-select text-center'><input class='ckbox' type='checkbox'></td>");
+});
+
+// Widen search input field
+$(".form-control.search-bar").css("width","455px");
+
+// Add 'Download all/selected' button to page
+if(document.getElementById("torrent-all") == null){
+	$(".nav.navbar-nav.navbar-right").append("<li><button style='margin-top: 9px' class='btn btn-primary' id='torrent-all'>Download</button></li>");
+	document.getElementById("torrent-all").addEventListener('click', function(){
+        if(confirm("This process cannot be stopped. Are you sure you want to download the selected files?")){
+            torrentAll();
+        }
+    }, false);
+}
+
+// Add event listener to select all checkbox
+$("#select-all").click(function() {
+    // Check/uncheck all checkboxes in the table rows
+    $("table.torrent-list tbody tr .ckbox").prop("checked", $(this).prop("checked"));
+});
