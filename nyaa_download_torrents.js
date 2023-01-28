3a5f99cd8557513c6c1b6f48e674b22a5277b83b
SeiyaGame <flavien.2001a@gmail.com>
2023-01-29 00:11:02 +0100

    incrementing the version number

--- nyaa_download_torrents.js
+++ nyaa_download_torrents.js
@@ -5,7 +5,7 @@
 // @homepage    https://github.com/SeiyaGame/NyaaTools
 // @updateURL   https://raw.githubusercontent.com/SeiyaGame/NyaaTools/main/nyaa_download_torrents.js
 // @downloadURL https://raw.githubusercontent.com/SeiyaGame/NyaaTools/main/nyaa_download_torrents.js
-// @version     1.5.0
+// @version     1.5.1
 // @grant       none
 // @require     https://code.jquery.com/jquery-3.5.1.min.js
 // @include     *nyaa.si/*
