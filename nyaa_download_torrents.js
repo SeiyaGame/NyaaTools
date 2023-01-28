b7397222b967eaa302d84ccfd770ed38802dafe8
SeiyaGame <flavien.2001a@gmail.com>
2023-01-29 00:09:09 +0100

    Correction of the color in the table

--- nyaa_download_torrents.js
+++ nyaa_download_torrents.js
@@ -14,10 +14,18 @@
 // @exclude     *nyaa.si/upload
 // ==/UserScript==
 
+// we redefine the colors of the table
+var css = document.createElement("style");
+css.setAttribute("type", "text/css");
+css.innerHTML = "table.torrent-list td:nth-child(6), body.dark table.torrent-list > tbody > tr.success > td:nth-child(6), body.dark table.torrent-list > tbody > tr.danger > td:nth-child(6) { color: #CBCBCB !important; } \
+                table.torrent-list td:nth-child(7), body.dark table.torrent-list > tbody > tr.success > td:nth-child(7), body.dark table.torrent-list > tbody > tr.danger > td:nth-child(7) { color: green !important; } \
+                table.torrent-list td:nth-child(8), body.dark table.torrent-list > tbody > tr.success > td:nth-child(8), body.dark table.torrent-list > tbody > tr.danger > td:nth-child(8) { color: red !important; }";
+document.head.appendChild(css);
+
 function torrentAll() {
     const $torrentRows = $("table.torrent-list tbody tr:has(.ckbox:checked)");
 
-    $torrentRows.each(function() {
+    $torrentRows.each(function () {
         const $downloadLink = $("a[href*='/download']", this);
         const $viewLink = $("a[href*='/view']", this);
 
@@ -40,25 +48,25 @@ function torrentAll() {
 
 // Add checkbox to table header
 $("table.torrent-list thead tr").prepend("<th class='hdr-select text-center' style='width: 10px'><input type='checkbox' id='select-all'></th>");
-$("table.torrent-list tbody tr").each(function(){
-	$(this).prepend("<td class='row-select text-center'><input class='ckbox' type='checkbox'></td>");
+$("table.torrent-list tbody tr").each(function () {
+    $(this).prepend("<td class='row-select text-center'><input class='ckbox' type='checkbox'></td>");
 });
 
 // Widen search input field
-$(".form-control.search-bar").css("width","455px");
+$(".form-control.search-bar").css("width", "455px");
 
 // Add 'Download all/selected' button to page
-if(document.getElementById("torrent-all") == null){
-	$(".nav.navbar-nav.navbar-right").append("<li><button style='margin-top: 9px' class='btn btn-primary' id='torrent-all'>Download</button></li>");
-	document.getElementById("torrent-all").addEventListener('click', function(){
-        if(confirm("This process cannot be stopped. Are you sure you want to download the selected files?")){
+if (document.getElementById("torrent-all") == null) {
+    $(".nav.navbar-nav.navbar-right").append("<li><button style='margin-top: 9px' class='btn btn-primary' id='torrent-all'>Download</button></li>");
+    document.getElementById("torrent-all").addEventListener('click', function () {
+        if (confirm("This process cannot be stopped. Are you sure you want to download the selected files?")) {
             torrentAll();
         }
     }, false);
 }
 
 // Add event listener to select all checkbox
-$("#select-all").click(function() {
+$("#select-all").click(function () {
     // Check/uncheck all checkboxes in the table rows
     $("table.torrent-list tbody tr .ckbox").prop("checked", $(this).prop("checked"));
 });
